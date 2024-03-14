558e0c832396bfb46da765f7ce6e2f474c70ecd4
ltx <tongxi09@gmail.com>
2024-03-14 20:47:34 +0800

    simple_tag, has problem when discrete=False

--- 1.MAPPO_MPE/MAPPO_MPE_main.py
+++ 1.MAPPO_MPE/MAPPO_MPE_main.py
@@ -10,6 +10,12 @@ from make_env import make_env
 
 
 class Runner_MAPPO_MPE:
+    """
+    discrete = False时有问题, self.env.action_space[i] has no attribute "n"
+    discrete = True, action_dim = 5
+    discrete = False 连续动作空间，action_dim怎样获取？ action_dim的含义是什么，有没有被用到？
+    随便赋值action_dim，训练得到的效果很差，reward=6.66667或0，运动轨迹像是滑行一会儿就停了
+    """
     def __init__(self, args, env_name, number, seed):
         self.args = args
         self.env_name = env_name
@@ -19,18 +25,19 @@ class Runner_MAPPO_MPE:
         np.random.seed(self.seed)
         torch.manual_seed(self.seed)
         # Create env
-        self.env = make_env(env_name, discrete=True) # Discrete action space
+        self.env = make_env(env_name, discrete=False) # Discrete action space
         self.args.N = self.env.n  # The number of agents
         self.args.obs_dim_n = [self.env.observation_space[i].shape[0] for i in range(self.args.N)]  # obs dimensions of N agents
-        self.args.action_dim_n = [self.env.action_space[i].n for i in range(self.args.N)]  # actions dimensions of N agents
+        # self.args.action_dim_n = [self.env.action_space[i].n for i in range(self.args.N)]  # actions dimensions of N agents
         # Only for homogenous agents environments like Spread in MPE,all agents have the same dimension of observation space and action space
         self.args.obs_dim = self.args.obs_dim_n[0]  # The dimensions of an agent's observation space
-        self.args.action_dim = self.args.action_dim_n[0]  # The dimensions of an agent's action space
+        # self.args.action_dim = self.args.action_dim_n[0]  # The dimensions of an agent's action space
+        self.args.action_dim = 5 #?
         self.args.state_dim = np.sum(self.args.obs_dim_n)  # The dimensions of global state space（Sum of the dimensions of the local observation space of all agents）
         print("observation_space=", self.env.observation_space)
         print("obs_dim_n={}".format(self.args.obs_dim_n))
         print("action_space=", self.env.action_space)
-        print("action_dim_n={}".format(self.args.action_dim_n))
+        # print("action_dim_n={}".format(self.args.action_dim_n))
 
         # Create N agents
         self.agent_n = MAPPO_MPE(self.args)
